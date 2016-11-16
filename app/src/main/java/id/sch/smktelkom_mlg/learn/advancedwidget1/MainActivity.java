9645a9c49cba8b21630637385a8bea04745bbeb9
RasendriyaWidya <rasendriyawidya@gmail.com>
2016-11-16 13:48:52 +0700

    Initial commit

--- app/src/main/java/id/sch/smktelkom_mlg/learn/advancedwidget1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/advancedwidget1/MainActivity.java
@@ -2,6 +2,8 @@ package id.sch.smktelkom_mlg.learn.advancedwidget1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout llmain = (LinearLayout) findViewById(R.id.linearLayoutMain);
+        EditText etNama = new EditText(this);
+        llmain.addView(etNama);
+
     }
 }
